ed5d6cb391efcba77291c535f0834732e48c9bd2
l3w4idiee3 <lewisjassy43@gmail.com>
2023-06-27 23:03:40 +0300

    added a classname code

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 //Importing components
 import Form from './components/form';
+import TodoList from './components/TodoList';
 function App() {
   return (
     <div className="App">
@@ -9,6 +10,7 @@ function App() {
         <h1>Lewis Todo List</h1>
       </header>
       <Form />
+      <TodoList />
     </div>
   );
 }
