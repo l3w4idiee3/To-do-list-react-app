d635de9c68c033971e517dc23d7f28ff7f8ae77e
l3w4idiee3 <lewisjassy43@gmail.com>
2023-06-27 22:51:07 +0300

    modified:   public/index.html 	modified:   src/App.js 	modified:   src/components/form.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-
+import React from 'react';
 import './App.css';
-
+//Importing components
+import Form from './components/form';
 function App() {
   return (
     <div className="App">
       <header>
         <h1>Lewis Todo List</h1>
       </header>
+      <Form />
     </div>
   );
 }
